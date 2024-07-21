4d5a38fe3ced4654d8ea634432385c26c723a99a
zicozydailva <zicozydasilva70@gmail.com>
2024-07-21 21:35:51 +0100

    chore: validation pipe setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+import { AppModule } from './main.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
   await app.listen(3000);
 }
 bootstrap();
