6b0e9ba2333557d4981b3aa67794ce62d5d381f0
zicozydailva <zicozydasilva70@gmail.com>
2024-07-21 22:16:18 +0100

    chore: err handling

--- src/modules/slack/slack.service.ts
+++ src/modules/slack/slack.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 
 import { InjectRepository } from '@nestjs/typeorm';
 import { SlackMessage } from './entities';
+import { ErrorHelper } from 'src/helpers/error.utils';
 
 @Injectable()
 export class SlackService {
@@ -13,22 +14,26 @@ export class SlackService {
   ) {}
 
   async fetchSlackMessages() {
-    const response = await axios.get(
-      'https://slack.com/api/conversations.history',
-      {
-        headers: { Authorization: `Bearer ${process.env.SLACK_API_TOKEN}` },
-      },
-    );
+    try {
+      const response = await axios.get(
+        'https://slack.com/api/conversations.history',
+        {
+          headers: { Authorization: `Bearer ${process.env.SLACK_API_TOKEN}` },
+        },
+      );
 
-    const messages = response.data.messages;
+      const messages = response.data.messages;
 
-    for (const message of messages) {
-      const slackMessage = this.slackMessageRepository.create({
-        userId: message.user,
-        message: message.text,
-        timestamp: new Date(parseFloat(message.ts) * 1000),
-      });
-      await this.slackMessageRepository.save(slackMessage);
+      for (const message of messages) {
+        const slackMessage = this.slackMessageRepository.create({
+          userId: message.user,
+          message: message.text,
+          timestamp: new Date(parseFloat(message.ts) * 1000),
+        });
+        await this.slackMessageRepository.save(slackMessage);
+      }
+    } catch (error) {
+      ErrorHelper.BadRequestException(error);
     }
   }
 }
