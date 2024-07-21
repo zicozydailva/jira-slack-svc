25658ad9a69e97e1a4f98993b71fe9f78efc5b71
zicozydailva <zicozydasilva70@gmail.com>
2024-07-21 22:13:13 +0100

    feat: fetch slack message handler

--- src/modules/slack/slack.module.ts
+++ src/modules/slack/slack.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SlackService } from './slack.service';
 import { SlackController } from './slack.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SlackMessage } from './entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SlackMessage])],
   providers: [SlackService],
-  controllers: [SlackController]
+  controllers: [SlackController],
 })
 export class SlackModule {}
--- src/modules/slack/slack.service.ts
+++ src/modules/slack/slack.service.ts
@@ -1,4 +1,34 @@
+import axios from 'axios';
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+
+import { InjectRepository } from '@nestjs/typeorm';
+import { SlackMessage } from './entities';
 
 @Injectable()
-export class SlackService {}
+export class SlackService {
+  constructor(
+    @InjectRepository(SlackMessage)
+    private slackMessageRepository: Repository<SlackMessage>,
+  ) {}
+
+  async fetchSlackMessages() {
+    const response = await axios.get(
+      'https://slack.com/api/conversations.history',
+      {
+        headers: { Authorization: `Bearer ${process.env.SLACK_API_TOKEN}` },
+      },
+    );
+
+    const messages = response.data.messages;
+
+    for (const message of messages) {
+      const slackMessage = this.slackMessageRepository.create({
+        userId: message.user,
+        message: message.text,
+        timestamp: new Date(parseFloat(message.ts) * 1000),
+      });
+      await this.slackMessageRepository.save(slackMessage);
+    }
+  }
+}
