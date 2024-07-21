6813abe74ece423cac3579cec3cbae8e9c6e8971
zicozydailva <zicozydasilva70@gmail.com>
2024-07-21 22:03:47 +0100

    chore: jira issue dependency injection

--- src/modules/jira/jira.service.ts
+++ src/modules/jira/jira.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { JiraIssue } from './entities';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class JiraService {}
+export class JiraService {
+  constructor(
+    @InjectRepository(JiraIssue)
+    private readonly jiraIssueRepository: Repository<JiraIssue>,
+  ) {}
+}
